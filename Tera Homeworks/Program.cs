bfd437ecec51e63d7466972f08866eabaede00b0
mariamgabriela <mariam.gabrielashvili5@gmail.com>
2023-03-10 13:07:35 -0800

    this is new code

--- Tera Homeworks/Program.cs
+++ Tera Homeworks/Program.cs
@@ -1,26 +1,10 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-class Mariami
+﻿class Homework3
 {
-    /*გამარჯობა, მე ვარ მარიამი,
-     * ეს არის ჩემი პირველი დავალება
-     * */
-
+  
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, This is My firt Line!"); //ასე შემიძლია კომენტარის გაწერა
-        Console.WriteLine();                             // ამით შემიძლია ცარიელი სტრიქონი გამოვიყვანო.
-        Console.WriteLine("This is second line");
-
-
-
-        /// <summary>
-        /// ამ ფორმულით შემიძლია დავაკომენტარო მეთოდი
-        /// </summary>
-        static void secondmethod()
-        {
-            Console.WriteLine();
-        }
+        Console.WriteLine();
+   
     }
 
     
